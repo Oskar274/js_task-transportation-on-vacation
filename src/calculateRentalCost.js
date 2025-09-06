ba70ded1ebe6f0b2a55010087091830e3d7c19b0
Oskar274 <neron2006tom@gmail.com>
2025-09-06 17:19:12 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let sum = days * 40;
+  const smallTerm = 3;
+  const smallTermDiscount = 20;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+
+  if (days >= longTerm) {
+    sum -= longTermDiscount;
+  } else if (days >= smallTerm) {
+    sum -= smallTermDiscount;
+  }
+
+  return sum;
 }
 
 module.exports = calculateRentalCost;
